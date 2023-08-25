5205346b51507021db8ce72a482b0fb32d6082fc
andersonlouzada <anderson.s.louzada@gmail.com>
2023-08-25 10:50:21 -0300

    feat: cria products.model, connection e index nos models

---
+++ backend/src/models/products.model.js
@@ -0,0 +1,22 @@
+const camelize = require('camelize');
+const connection = require('./connection');
+
+const findAll = async () => {
+  const [products] = await connection.execute(
+    'SELECT * FROM products ORDER BY id',
+  );
+  return camelize(products);
+};
+
+const findByID = async (productId) => {
+  const [[product]] = await connection.execute(
+    'SELECT * FROM products WHERE id = ?',
+    [productId],
+  );
+  return camelize(product);
+};
+
+module.exports = {
+  findAll,
+  findByID,
+};
