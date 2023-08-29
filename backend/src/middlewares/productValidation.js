565c2b3469759e87ffc9fcb0d27263c5321254a5
andersonlouzada <anderson.s.louzada@gmail.com>
2023-08-29 09:12:43 -0300

    altera validações no productValidation de acordo com o requisito 04

--- backend/src/middlewares/productValidation.js
+++ backend/src/middlewares/productValidation.js
@@ -1,7 +1,10 @@
 const productValidation = (req, res, next) => {
   const { name } = req.body;
   if (!name || name === null) {
-    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
   }
   next();
 };
