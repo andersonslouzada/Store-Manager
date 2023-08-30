2846dbf084c8306202d530895762220db9353107
andersonlouzada <anderson.s.louzada@gmail.com>
2023-08-30 06:58:21 -0300

    refatora a função quantityValidation no middleware de validação de vendas

--- backend/src/middlewares/saleValidation.js
+++ backend/src/middlewares/saleValidation.js
@@ -8,34 +8,21 @@ const idValidation = (req, res, next) => {
 
 const quantityValidation = (req, res, next) => {
   const products = req.body;
-  const quantityExists = products.every((product) => 'quantity' in product);
-  const quantityCorrect = products.every((product) => 
-    'quantity' in product && product.quantity > 0);
-  if (!quantityExists) return res.status(400).json({ message: '"quantity" is required' });
-  if (!quantityCorrect) {
+
+  const missingQuantity = products.some((product) => !('quantity' in product));
+  const invalidQuantity = products.some((product) => 
+    'quantity' in product && product.quantity <= 0);
+
+  if (missingQuantity) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+
+  if (invalidQuantity) {
     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
   }
 
   next();
 };
-
-// const quantityValidation = (req, res, next) => {
-//   const products = req.body;
-
-//   const missingQuantity = products.some((product) => !('quantity' in product));
-//   const invalidQuantity = products.some((product) => 
-//     'quantity' in product && product.quantity <= 0);
-
-//   if (missingQuantity) {
-//     return res.status(400).json({ message: '"quantity" is required' });
-//   }
-
-//   if (invalidQuantity) {
-//     return res.status(422).json({ message: '"quantity" must be greater than 0' });
-//   }
-
-//   next();
-// };
 
 const saleValidation = [
   idValidation,
